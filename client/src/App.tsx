89dd9afa3dc27bba1243a9ac667a0a35441284d7
Tawan Sukapat <6510110158@email.psu.ac.th>
2023-03-08 17:24:19 +0700

    Fix remove contactcard  and history

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,6 @@
 import { BrowserRouter, Route, Routes} from 'react-router-dom'
 import MainPage from './pages/main/main';
 import Status from './pages/status/status';
-import History from './pages/history/history';
 import Signin from './pages/signin/signin';
 import MainLog from './pages/main/mainxlog';
 import Signup from './pages/signup/signup';
@@ -17,12 +16,10 @@ function App() {
         <Route index element={<MainLog/>} />      
         <Route path="/" element={<MainPage />}  />
         <Route path="/status" element={<Status />} />
-        <Route path="/history" element={<History />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/information" element={<Information />} />
         <Route path="/information/:id" element={<Information />} />
-
       </Routes>
     </BrowserRouter>
 
