51c1b22fcd761d3dea046b92a7862b3bf578db0c
Siwakorn Noylam <6510110465@gmail.psu.ac.th>
2023-03-08 22:35:07 +0700

    conect contact

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,7 @@ import Signin from './pages/signin/signin';
 import MainLog from './pages/main/mainxlog';
 import Signup from './pages/signup/signup';
 import Information from './pages/information/information';
-
+import Contact from './pages/contact/contact';
 
 
 function App() {
@@ -17,9 +17,11 @@ function App() {
         <Route path="/" element={<MainPage />}  />
         <Route path="/status" element={<Status />} />
         <Route path="/signin" element={<Signin />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/information" element={<Information />} />
         <Route path="/information/:id" element={<Information />} />
+        
       </Routes>
     </BrowserRouter>
 
