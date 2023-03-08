b3a641e13e139601f0a3bbed8584fe877f6d0d3d
Phoorin Nickphong <6510110371@psu.ac.th>
2023-03-09 04:04:38 +0700

    create activity register handle

--- client/src/pages/information/information.tsx
+++ client/src/pages/information/information.tsx
@@ -4,30 +4,193 @@ import ActivityAppBar from "../../components/app-bar";
 import kitjakum from "../../Models/kitjakum";
 import Repo from "../../Repository/index";
 import { useNavigate, useParams } from "react-router-dom";
+import Swal from "sweetalert2";
 
+const getUserData = () => {
+  const stringfiedUser = localStorage.getItem("user") || "";
+  if (stringfiedUser) {
+    return JSON.parse(stringfiedUser);
+  }
+  return false;
+};
 
 const Information = () => {
   const [userresult, setUserResult] = useState<kitjakum[]>([]);
-  const navigate = useNavigate(); 
+  const [activityresult, setActivityResult] = useState<kitjakum[]>([]);
+  const navigate = useNavigate();
   const params = useParams();
-
+  const [isRegis, setIsRegis] = useState(false)
+  const userData = getUserData();
   const data = userresult.length > 0 ? userresult[0].attributes : null;
   const thumbnail = `http://localhost:1337${data?.Image.data[0].attributes.url}`;
+
 
   const fetchData = async () => {
     try {
       const res = await Repo.userResult.getById(params.id as string);
+      const data2 = await fetch(`http://localhost:1337/api/first-come-first-serves?filters[Username]=${userData.username}&filters[ActivityID]=${params.id}`)
+
       if (res) {
         setUserResult([res])
       }
+      if (data2) {
+        const data3 = await data2.json();
+        setActivityResult(data3.data);
+      }
+
     } catch (error) {
       console.log(error)
+    }
+  };
+
+
+
+  async function SignupClick(id: string) {
+    const activity = {
+      data: {
+        title: data?.title,
+        Username: userData.username,
+        ActivityID: params.id,
+      },
+    };
+
+    const requestBody = JSON.stringify(activity);
+    if (data?.Typeregister == "First Come First Serve") {
+      if (userData) {
+        const result = await Swal.fire({
+          title: "Are you sure?",
+          text: "Do you really want to attend this activity?",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#1bcc32",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Confirm",
+          cancelButtonText: "Cancel",
+        });
+
+        if (result.isConfirmed) {
+          try {
+            const resp = await fetch(
+              `http://localhost:1337/api/activity/${id}/like`,
+              {
+                method: "GET",
+                headers: {
+                  "Content-Type": "application/json",
+                  Authorization: `Bearer ${userData.jwt}`,
+                },
+              }
+            );
+            const data = await resp.json();
+            console.log(data);
+            const response = await fetch(
+              "http://localhost:1337/api/first-come-first-serves",
+              {
+                method: "POST",
+                headers: {
+                  Authorization: `Bearer ${userData.jwt}`,
+                  "Content-Type": "application/json",
+                },
+                body: requestBody,
+              }
+            );
+
+            const data1 = await response.json();
+            console.log(data1);
+            fetchData();
+            Swal.fire({
+              title: "Applied ",
+              text: "applied successfully",
+              icon: "success",
+              confirmButtonText: "OK",
+            });
+            setIsRegis(true);
+          } catch (err) {
+            console.error(err);
+          }
+        }
+      } else {
+        Swal.fire({
+          title: "You have no access",
+          text: "plese signin first",
+          icon: "error",
+          confirmButtonText: "OK",
+        }).then(() => {
+          navigate("/signin");
+        });
+      }
+    } else {
+      if (userData) {
+        const result = await Swal.fire({
+          title: "Are you sure?",
+          text: "Do you really want to attend this activity?",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#1bcc32",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Confirm",
+          cancelButtonText: "Cancel",
+        });
+
+        if (result.isConfirmed) {
+          try {
+            const resp = await fetch(
+              `http://localhost:1337/api/activity/${id}/like`,
+              {
+                method: "GET",
+                headers: {
+                  "Content-Type": "application/json",
+                  Authorization: `Bearer ${userData.jwt}`,
+                },
+              }
+            );
+            const data = await resp.json();
+            console.log(data);
+            const response = await fetch(
+              "http://localhost:1337/api/candidates",
+              {
+                method: "POST",
+                headers: {
+                  Authorization: `Bearer ${userData.jwt}`,
+                  "Content-Type": "application/json",
+                },
+                body: requestBody,
+              }
+            );
+
+            const data1 = await response.json();
+            console.log(data1);
+            fetchData();
+            Swal.fire({
+              title: "Applied ",
+              text: "applied successfully",
+              icon: "success",
+              confirmButtonText: "OK",
+            });
+            setIsRegis(true);
+          } catch (err) {
+            console.error(err);
+          }
+        }
+      } else {
+        Swal.fire({
+          title: "You have no access",
+          text: "plese signin first",
+          icon: "error",
+          confirmButtonText: "OK",
+        }).then(() => {
+          navigate("/login");
+        });
+      }
     }
   }
 
   useEffect(() => {
     fetchData()
   }, [params.id])
+
+  // useEffect(() => {
+  //   handleDisabled()
+  // })
 
   return (
     <html>
@@ -46,13 +209,13 @@ const Information = () => {
       <body>
         <ActivityAppBar></ActivityAppBar>
         <header className="isOpen">
-          <h1>เปิดรับอยุ่นะจุ๊บๆ</h1>
+          <h1>กิจกรรม</h1>
         </header>
         <div className="images">
           <img className="picture"
             src={thumbnail}
             alt="activity image"
-            /* style={{ objectFit: 'cover', height: 140 }} */
+          /* style={{ objectFit: 'cover', height: 140 }} */
           />
 
         </div>
@@ -80,9 +243,10 @@ const Information = () => {
         </ul>
         <br />
         <div className="box56">
-        <button className="Button20 font" type="submit">
-          ลงทะเบียน
-        </button>
+          <button className="Button20 font" type="submit" onClick={() =>
+            params.id && SignupClick(params.id.toString())}>
+            ลงทะเบียน
+          </button>
         </div>
       </body>
     </html>
